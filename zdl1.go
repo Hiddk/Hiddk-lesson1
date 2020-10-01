428bf31ba3309591ad9f77682bde0122d966405b
Hiddk <72072961+Hiddk@users.noreply.github.com>
2020-10-01 23:09:35 +0300

    All files

---
+++ zdl1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"https://github.com/Hiddk/lesson1"
+)
+const usd.float64 = 79,89
+
+func main(){
+	var rub float64c
+	fmt.Println ("Введите сумму в рублях для конвертации в доллар")
+	fmt.Scanln (&rub)
+	var conv.float64 = rub/usd
+	fmt.Println ("Вы получите -" conv)
+}
+
